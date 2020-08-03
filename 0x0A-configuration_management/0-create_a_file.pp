0f142a9d339f9abdce51b8a7b92373e8a38f9e0d
hunterxx0 <azouzimhamed@gmail.com>
2020-08-03 16:11:28 +0000

    first

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create a file in /tmp
+file { '/tmp/holberton':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
