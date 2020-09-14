69ee7ee30022a917e98680f42412a27c1afb83b8
hunterxx0 <azouzimhamed@gmail.com>
2020-09-14 19:32:52 +0000

    first

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Fetches https://intranet.hbtn.io/status """
+from json import loads, dumps
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    user_id = eval(argv[1])
+    res = get('https://jsonplaceholder.typicode.com/todos').text
+    user = get('https://jsonplaceholder.typicode.com/users/{}'
+               .format(user_id)).text
+    dict_list = loads(res)
+    result = []
+    username = eval(user).get('username')
+    for dic in dict_list:
+        if dic['userId'] == user_id:
+            tmp = dic.copy()
+            del tmp["id"]
+            del tmp["userId"]
+            tmp["username"] = username
+            result.append(tmp)
+    with open('{}.json'.format(user_id), mode='w', encoding='utf-8') as f:
+        f.write(dumps({user_id: result}))
