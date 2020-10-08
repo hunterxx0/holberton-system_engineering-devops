8be9fbf1ecf07010c4b7f5d5a86456ff7454f5e8
hunterxx0 <azouzimhamed@gmail.com>
2020-10-08 19:32:11 +0000

    first

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # .php
 exec { 'setting.php':
   path    =>  ['/bin'],
-  command =>  'rm -f abcdef',
+  command =>  'sudo sed -i -e "$aULIMIT=\"-n 4096\"" /etc/default/nginx ; sudo service nginx restart',
   }
